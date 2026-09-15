8c009b74a74306d1198eed4efbb15b030c939f8d
Bruno Roy <broy@cloudflare.com>
2026-09-14 17:55:22 -0700

    Fix Clippy warnings on Rust 1.98

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -4744,7 +4744,7 @@ impl<'a> Parser<'a> {
         if self.parse_keyword(expected) {
             Ok(self.get_current_token().clone())
         } else {
-            self.expected_ref(format!("{:?}", &expected).as_str(), self.peek_token_ref())
+            self.expected_ref(format!("{:?}", expected).as_str(), self.peek_token_ref())
         }
     }
 
@@ -4757,7 +4757,7 @@ impl<'a> Parser<'a> {
         if self.parse_keyword(expected) {
             Ok(())
         } else {
-            self.expected_ref(format!("{:?}", &expected).as_str(), self.peek_token_ref())
+            self.expected_ref(format!("{:?}", expected).as_str(), self.peek_token_ref())
         }
     }
 
@@ -13375,16 +13375,14 @@ impl<'a> Parser<'a> {
                                 "Trailing period in identifier".to_string(),
                             ))?
                         }
-                        token => {
-                            return Err(ParserError::ParserError(format!(
-                                "Unexpected token following period in identifier: {token}"
-                            )))?
-                        }
+                        token => Err(ParserError::ParserError(format!(
+                            "Unexpected token following period in identifier: {token}"
+                        )))?,
                     }
                 }
                 Token::EOF => break,
                 token => {
-                    return Err(ParserError::ParserError(format!(
+                    Err(ParserError::ParserError(format!(
                         "Unexpected token in identifier: {token}"
                     )))?;
                 }
@@ -16643,7 +16641,7 @@ impl<'a> Parser<'a> {
                 where_clause = Some(self.parse_expr()?);
             } else {
                 let tok = self.peek_token_ref();
-                return parser_err!(
+                parser_err!(
                     format!(
                         "Expected one of DIMENSIONS, METRICS, FACTS or WHERE, got {}",
                         tok.token
